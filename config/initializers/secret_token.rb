caa61c160219add505fe6c391a24d86b72c9cfbd
ae <andrewaka39@gmail.com>
2012-01-17 12:57:46 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artzavtra::Application.config.secret_token = '573d1e95eac6a00e36775f2f1e02fd39a2c8141eb3920ae8c7b5e9120113cf8f28ba9a5dda014b79d06f84c97d9dcaad4a77740933c0ca4a759a70282319d114'
